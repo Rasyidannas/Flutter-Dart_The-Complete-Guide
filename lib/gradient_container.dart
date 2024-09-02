0a27cb0d3fbf70069258ad4880ca5c759c2abf40
rasshit.dsgn <rasshit.dsgn@gmail.com>
2024-09-02 02:20:59 +0000

    2/42. final and const - special kind of variables.mp4

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/style_text.dart';
 
-var startAlignment = Alignment.topLeft;
-var endAlignment = Alignment.bottomRight;
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 //custom widget
 class GradientContainer extends StatelessWidget {
@@ -11,9 +11,9 @@ class GradientContainer extends StatelessWidget {
   @override
   Widget build(context) {
     return Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         gradient: LinearGradient(
-          colors: const [
+          colors: [
             Color.fromARGB(255, 26, 2, 80),
             Color.fromARGB(255, 45, 7, 98)
           ],

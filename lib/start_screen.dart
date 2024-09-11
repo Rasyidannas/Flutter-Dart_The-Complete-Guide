ae97fc4765b7e48e53dbb2a4ffa72b357e761d68
rasshit.dsgn <rasshit.dsgn@gmail.com>
2024-09-11 03:00:14 +0000

    3/54. Challenge Solution 1/2 - Creating a Widget.mp4

---
+++ lib/start_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+//custom widget
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return const Text("Start Screen");
+  }
+}

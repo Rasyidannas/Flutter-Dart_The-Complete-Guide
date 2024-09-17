ce4c5395a312ab79ae5302288bc4f46223631e48
rasshit.dsgn <rasshit.dsgn@gmail.com>
2024-09-17 01:21:29 +0000

    3/58. Repeating and Exercise: Adding a Stateful Widget.mp4

---
+++ lib/questions_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key})
+
+  @override
+  State<QuestionsScreen> createState() {
+    return _QuestionsScreenState();
+  }
+}
+
+// private class
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  @override
+  Widget build(context) {
+    return Text('QuestionScreen')
+  }
+}

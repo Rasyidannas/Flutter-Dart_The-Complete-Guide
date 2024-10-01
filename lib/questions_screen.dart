593b2de02005d4790df46b399df9432e824143a3
rasyidannas <rasshit.dsgn@gmail.com>
2024-10-01 08:27:10 +0700

    3/71. Mapping Lists & Using the Spread Operator.mp4

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -29,22 +29,10 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             style: const TextStyle(color: Colors.white),
           ),
           const SizedBox(height: 30),
-          AnswerButton(
-            answerText: currentQuestion.answers[0],
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: currentQuestion.answers[1],
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: currentQuestion.answers[2],
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: currentQuestion.answers[3],
-            onTap: () {},
-          ),
+          //this is spread operator like in JS, it will spread the array to the
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(answerText: answer, onTap: () {});
+          }),
         ],
       ),
     );

431f07976b2df0d248a5f170479a0e92126ab22c
rasshit.dsgn <rasshit.dsgn@gmail.com>
2024-10-10 02:42:07 +0000

    3/76. using third-party package & adding google fonts.mp4

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 //custom widget
 class StartScreen extends StatelessWidget {
@@ -21,10 +22,10 @@ class StartScreen extends StatelessWidget {
           const SizedBox(
             height: 80,
           ),
-          const Text(
+          Text(
             'Learn Flutter the fun way!',
-            style: TextStyle(
-              color: Color.fromARGB(255, 237, 223, 252),
+            style: GoogleFonts.lato(
+              color: const Color.fromARGB(255, 237, 223, 252),
               fontSize: 24,
             ),
           ),
